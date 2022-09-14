0473e66fc8480453167fe023dbacde0e13ce5ab7
ZeusGolohor <golohorzeusufuoma@gmail.com>
2022-09-14 06:30:04 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 6-main.c 6-abs.c -o 6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _abs - Computes absolute
+ * value of an integer
+ * @n: First operand
+ * Return: An integer absolute
+ * value.
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
