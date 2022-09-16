c15d56a06e258a3f8aae29dc3a1b058c4c70d2ab
ZeusGolohor <golohorzeusufuoma@gmail.com>
2022-09-15 17:00:27 -0700

    The “Fizz-Buzz test” is an interview question designed to help filter out the 99.5% of programming job candidates who can’t seem to program their way out of a wet paper bag.
    Write a program that prints the numbers from 1 to 100, followed by a new line. But for multiples of three print Fizz instead of the number and for the multiples of five print Buzz. For numbers which are multiples of both three and five print FizzBuzz.
    
    Each number or word should be separated by a space
    You are allowed to use the standard library

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int number;
+
+	number = 1;
+	while (number <= 100)
+	{
+		if ((number % 3) == 0 && (number % 5) == 0)
+			printf("FizzBuzz ");
+		else if ((number % 5) == 0)
+		       printf("Buzz ");
+		else if ((number % 3) == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", number);
+		number++;
+	}
+	printf("\n");
+	return (0);
+}
