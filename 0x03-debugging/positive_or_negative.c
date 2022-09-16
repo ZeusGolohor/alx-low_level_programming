e5feeec76e94ba4fb7566293f0ed0290d4b06d51
ZeusGolohor <golohorzeusufuoma@gmail.com>
2022-09-15 20:06:44 -0700

    In most projects, we often give you only one main file to test with. For example, this main file is a test for a postitive_or_negative() function similar to the one you worked with in an earlier C project: Based on the main.c file above, create a file named 0-main.c. This file must test that the function positive_or_negative() gives the correct output when given a case of 0.
    You are not coding the solution / function, you’re just testing it! However, you can adapt your function from 0x01. C - Variables, if, else, while - Task #0 to compile with this main file to test locally.
    
    You only need to upload 0-main.c and main.h for this task. We will provide our own positive_or_negative() function.
    You are not allowed to add or remove lines of code, you may change only one line in this task.

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * positive_or_negative - To print.
+ * negative or positive number
+ * @i: First operand
+ * Return: Always an int.
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
+	else
+		printf("%d is negative\n", i);
+}
