90d6f8541a723a12c0b2a2842435045823f06ba0
ZeusGolohor <golohorzeusufuoma@gmail.com>
2022-09-16 18:04:13 -0700

    Write a function that prints an integer.
    Prototype: void print_number(int n);
    You can only use _putchar function to print
    You are not allowed to use long
    You are not allowed to use arrays or pointers
    You are not allowed to hard-code special values

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - Entry point.
+ * print_number - Used to
+ * print numbers.
+ * @n: First operand.
+ * Return: Always an int.
+ */
+void print_number(int n)
+{
+	unsigned int y;
+
+	y = n;
+	if (n < 0)
+	{
+		_putchar(45);
+		y = -n;
+	}
+	if (y / 10)
+		print_number(y / 10);
+	_putchar((y % 10) + '0');
+}
