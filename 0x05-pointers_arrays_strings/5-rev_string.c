7c54515775f85a0765caed4e137c42421eb43282
ZeusGolohor <golohorzeusufuoma@gmail.com>
2022-09-21 15:40:21 -0700

    Write a function that prints half of a string, followed by a new line.
    Prototype: void puts_half(char *str);
    The function should print the second half of the string
    If the number of characters is odd, the function should print the last n characters of the string, where n = (length_of_the_string - 1) / 2

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,30 +1,14 @@
 #include "main.h"
 #include <string.h>
-#include <stdio.h>
 
 /**
- * rev_string - reserve a string
+ * rev_string - print string
+ * in reverse format
  * @s: first operand
  * Return: void
-*/
+ */
+
 void rev_string(char *s)
 {
-	int i;
-	int strLen = strlen(s);
-	char oldS[10];
-/**	int oldSLen;*/
-
-	for (i = 0; i <= strLen; i++)
-	{
-		oldS[i] = *(s + i);
-	}
-
-	/**oldSLen = strlen(oldS);*/
-	for (i = 1; i <= strLen; i++)
-	{
-		if (*(oldS + (strlen(oldS) - i)) != '0')
-			*(s + i) = *(oldS + (strlen(oldS) - i));
-	}
-
-	
+	strrev(s);
 }
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts_half - prints second
+ * half a given string
+ * @str: first operand
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int strLen, n, i;
+
+	strLen = strlen(str);
+	n = strLen / 2;
+	for (i = n; i < strLen; i++)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
