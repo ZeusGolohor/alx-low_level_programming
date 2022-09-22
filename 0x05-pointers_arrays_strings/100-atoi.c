4e13379aaded09bf41d0935783c3c239b89bc7f4
ZeusGolohor <golohorzeusufuoma@gmail.com>
2022-09-21 19:23:26 -0700

    Write a function that prints half of a string, followed by a new line.
    Prototype: void puts_half(char *str);
    The function should print the second half of the string
    If the number of characters is odd, the function should print the last n characters of the string, where n = (length_of_the_string - 1) / 2

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _atoi - convert sting to
+ * integer
+ * @s: first operand
+ * Return: Always an int.
+ */
+
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,13 +11,27 @@
 
 void puts_half(char *str)
 {
-	int strLen, n, i;
+	int len, n, i;
 
-	strLen = strlen(str);
-	n = strLen / 2;
-	for (i = n; i <  strLen; i++)
+	len = 0;
+
+	while (str[len] != '\0')
 	{
-		_putchar(*(str + i));
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	} else if (len % 2)
+	{
+		for (n = (len - 1) / 2; n < len - 1; n++)
+		{
+			_putchar(str[n + 1]);
+		}
 	}
 	_putchar('\n');
 }
