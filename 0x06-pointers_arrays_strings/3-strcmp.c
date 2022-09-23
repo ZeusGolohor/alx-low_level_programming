100b4e005b42fbc09b49c7c6e98cea5b7c6e8850
ZeusGolohor <golohorzeusufuoma@gmail.com>
2022-09-22 17:37:30 -0700

    Write a function that compares two strings.
    Prototype: int _strcmp(char *s1, char *s2);
    Your function should work exactly like strcmp
    FYI: The standard library provides a similar function: strcmp. Run man strcmp to learn more.

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - Used to check equality of
+ * of two strings
+ * @s1: first operand.
+ * @s2: second operand.
+ * Return: Always an int.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
