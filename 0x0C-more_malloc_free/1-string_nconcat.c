f91a0c8f854e71c8338c0e6d317d635dfd4eebd5
ZeusGolohor <golohorzeusufuoma@gmail.com>
2022-10-07 20:18:03 -0700

    Write a function that concatenates two strings.
    Prototype: char *string_nconcat(char *s1, char *s2, unsigned int n);
    The returned pointer shall point to a newly allocated space in memory, which contains s1, followed by the first n bytes of s2, and null terminated
    If the function fails, it should return NULL
    If n is greater or equal to the length of s2 then use the entire string s2
    if NULL is passed, treat it as an empty string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -50,9 +50,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *str;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		s1 = "";
 	if (s2 == NULL)
-		s2 = '\0';
+		s2 = "";
 	if (s1 != NULL)
 		len_s1 = strlen(s1);
 	if (s2 != NULL)
