c5dcdaae331d22b39bf116f532311664c0218dbd
ZeusGolohor <golohorzeusufuoma@gmail.com>
2022-10-07 18:11:47 -0700

    Write a function that concatenates two strings.
    Prototype: char *string_nconcat(char *s1, char *s2, unsigned int n);
    The returned pointer shall point to a newly allocated space in memory, which contains s1, followed by the first n bytes of s2, and null terminated
    If the function fails, it should return NULL
    If n is greater or equal to the length of s2 then use the entire string s2
    if NULL is passed, treat it as an empty string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,41 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+
+/**
+  * loop - used to loop through values
+  * @s1: first operand.
+  * @s2: second operand.
+  * @str: third operand.
+  * @i: forth operand.
+  * @x: fifth operand.
+  * Return: void.
+  */
+void loop(char *s1, char *s2, char *str, unsigned int i, unsigned int x)
+{
+	if (s1 != NULL)
+	{
+		while (*(s1 + i) != '\0')
+		{
+			*(str + i) = *(s1 + i);
+			i++;
+		}
+		if (s2 == NULL)
+			*(str + i) = '\0';
+	}
+	if (s2 != NULL)
+	{
+		while (*(s2 + x) != '\0')
+		{
+			*(str + i) = *(s2 + x);
+			x++;
+			i++;
+		}
+		if (s2 != NULL)
+			*(str + i) = '\0';
+	}
+}
 
 /**
   * string_nconcat - Used to concatenate two strings
@@ -12,38 +47,20 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len_s1 = 0, len_s2 = 0, len_total = 0, i = 0, x;
-	char *str = malloc((sizeof(char) * (strlen(s1) + n)) + 1);
-
+	unsigned int len_s1 = 0,len_s2 = 0, len_total = 0, i = 0, x = 0;
+	char *str;
+	
+	if (s1 != NULL)
+		len_s1 = strlen(s1);
+	if (s2 != NULL)
+		if (n >= strlen(s2))
+			len_s2 = strlen(s2);
+		else
+			len_s2 = n;
+	len_total = len_s1 + len_s2;
+	str = malloc(sizeof(char) * (len_total + 1));
 	if (str == NULL)
 		return (NULL);
-	len_s1 = strlen(s1);
-	len_s2 = n;
-	len_total = len_s1 + len_s2;
-	i = 0;
-	while (i < len_s1)
-	{
-		*(str + i) = *(s1 + i);
-		i++;
-	}
-	if (s2 == NULL)
-	{
-		*(str + i) = '\0';
-		return (str);
-	}
-	if (*s2 != '\0')
-	{
-		x = 0;
-		while (i < len_total)
-		{
-			*(str + i) = *(s2 + x);
-			x++;
-			i++;
-		}
-		if (s1 == NULL)
-		{
-			*(str + i) = '\0';
-		}
-	}
+	loop(s1, s2, str, i, x);
 	return (str);
 }
