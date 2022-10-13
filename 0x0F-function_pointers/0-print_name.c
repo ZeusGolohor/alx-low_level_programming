787696f90f1f4229d428b07db28ebe401e94f965
ZeusGolohor <golohorzeusufuoma@gmail.com>
2022-10-12 20:32:56 -0700

    Write a function that prints a name.
    Prototype: void print_name(char *name, void (*f)(char *));

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-#include <string.h>
+#include <stddef.h>
 
 /**
   * print_name - prints a name
@@ -10,6 +10,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL || f != NULL)
-		f(name);
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
 }
