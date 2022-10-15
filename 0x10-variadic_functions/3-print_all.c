38d5f2aac15285ef7d810548446446c47773c5c5
ZeusGolohor <golohorzeusufuoma@gmail.com>
2022-10-15 09:14:07 -0700

    Write a function that prints anything.
    Prototype: void print_all(const char * const format, ...);
    where format is a list of types of arguments passed to the function
    c: char
    i: integer
    f: float
    s: char * (if the string is NULL, print (nil) instead
    any other char should be ignored
    see example
    You are not allowed to use for, goto, ternary operator, else, do ... while
    You can use a maximum of
    2 while loops
    2 if
    You can declare a maximum of 9 variables
    You are allowed to use printf
    Print a new line at the end of your function

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -59,13 +59,7 @@ void sw(const char * const format, unsigned int i,
 			break;
 		case 's':
 			str = va_arg(list, char *);
-			if (str == NULL)
-			{
-				printf("(nil)");
-				iff(i, len);
-				break;
-			}
-			printf("%s", str);
+			((str == NULL)? printf("(nil)"): printf("%s", str));
 			iff(i, len);
 			break;
 	}
