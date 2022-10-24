2004b3bfc91e380fa8a1712f63fa349f67d6cdb0
ZeusGolohor <golohorzeusufuoma@gmail.com>
2022-10-24 14:11:49 -0700

    Write a function that frees a list_t list.
    Prototype: void free_list(list_t *head);

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,19 +10,22 @@ void free_list(list_t *head)
 {
 	list_t *temp = head, *current;
 
-	while (temp->next != NULL)
+	if (head != NULL)
 	{
-		current = temp;
-		temp = temp->next;
-		free(current->str);
-		free(current);
-	}
-	if (temp->next == NULL)
-	{
-		current = temp;
-		temp = temp->next;
-		free(current->str);
-		free(current);
-		free(temp);
+		while (temp->next != NULL)
+		{
+			current = temp;
+			temp = temp->next;
+			free(current->str);
+			free(current);
+		}
+		if (temp->next == NULL)
+		{
+			current = temp;
+			temp = temp->next;
+			free(current->str);
+			free(current);
+			free(temp);
+		}
 	}
 }
