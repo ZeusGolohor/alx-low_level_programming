ee1fcb59cc63b37f39a2cdd571c8596c68b4d0a8
ZeusGolohor <golohorzeusufuoma@gmail.com>
2022-10-26 19:11:29 -0700

    Write a function that returns the nth node of a listint_t linked list.
    Prototype: listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
    where index is the index of the node, starting at 0
    if the node does not exist, return NULL

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+  * get_nodeint_at_index - Used to traverse a singly linked list to locate a
+  * node using an index.
+  * @head: listint_t
+  * @indec: unsigned int
+  * Return: listint_t *
+  */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	if (head == NULL)
+		return (NULL);
+
+	while (head != NULL)
+	{
+		if (i == index)
+		{
+			return (head);
+		}
+		i++;
+		head = head->next;
+	}
+	return (NULL);
+}
