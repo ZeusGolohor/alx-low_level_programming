269b62f202d2cdfea5a134a67fdd6318b367464f
ZeusGolohor <golohorzeusufuoma@gmail.com>
2022-11-04 19:50:21 -0700

    Write a program that copies the content of a file to another file.
    Usage: cp file_from file_to
    if the number of argument is not the correct one, exit with code 97 and print Usage: cp file_from file_to, followed by a new line, on the POSIX standard error
    if file_to already exists, truncate it
    if file_from does not exist, or if you can not read it, exit with code 98 and print Error: Can't read from file NAME_OF_THE_FILE, followed by a new line, on the POSIX standard error
    where NAME_OF_THE_FILE is the first argument passed to your program
    if you can not create or if write to file_to fails, exit with code 99 and print Error: Can't write to NAME_OF_THE_FILE, followed by a new line, on the POSIX standard error
    where NAME_OF_THE_FILE is the second argument passed to your program
    if you can not close a file descriptor , exit with code 100 and print Error: Can't close fd FD_VALUE, followed by a new line, on the POSIX standard error
    where FD_VALUE is the value of the file descriptor
    Permissions of the created file: rw-rw-r--. If the file already exists, do not change the permissions
    You must read 1,024 bytes at a time from the file_from to make less system calls. Use a buffer
    You are allowed to use dprintf

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,4 +13,5 @@ int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char);
 int append_text_to_file(const char *filename, char *text_content);
+void _cp(const char *file_from, const char *file_to);
 #endif
