ac360a3576868119b879971eec5093df7c59921b
ZeusGolohor <golohorzeusufuoma@gmail.com>
2023-02-08 17:19:38 -0800

    Write a function that creates a hash table.
    Prototype: hash_table_t *hash_table_create(unsigned long int size);
    where size is the size of the array
    Returns a pointer to the newly created hash table
    If something went wrong, your function should return NULL

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,6 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
+	table->size = size;
 	table->array = calloc(sizeof(hash_node_t *), size);
 	if (table->array == NULL)
 		return (NULL);
