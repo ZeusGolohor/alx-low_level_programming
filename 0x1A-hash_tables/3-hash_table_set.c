64890bcef0e2c099662ab599c22d53a29b76357f
ZeusGolohor <golohorzeusufuoma@gmail.com>
2023-02-11 12:32:48 -0800

    Write a function that adds an element to the hash table.
    Prototype: int hash_table_set(hash_table_t *ht, const char *key, const char *value);
    Where ht is the hash table you want to add or update the key/value to
    key is the key. key can not be an empty string
    and value is the value associated with the key. value must be duplicated. value can be an empty string
    Returns: 1 if it succeeded, 0 otherwise
    In case of collision, add the new node at the beginning of the list

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,6 +12,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *newnode, *temp;
 
+	if (ht == NULL)
+		return (0);
 	if (strlen(key) == 0)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
