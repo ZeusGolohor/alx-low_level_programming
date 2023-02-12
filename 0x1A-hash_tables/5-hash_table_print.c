6c89c1a374f45be9572b305cede807fceb56778f
ZeusGolohor <golohorzeusufuoma@gmail.com>
2023-02-11 16:12:47 -0800

    Write a function that prints a hash table.
    Prototype: void hash_table_print(const hash_table_t *ht);
    where ht is the hash table
    You should print the key/value in the order that they appear in the array of hash table
    Order: array, list
    Format: see example
    If ht is NULL, don’t print anything

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,13 +1,14 @@
 #include "hash_tables.h"
 
 /**
-  * hash_table_prhash_table_printint - Used to print an hash table.
+  * hash_table_print - Used to print an hash table.
   * @ht: pointer to hash table.
   * Return: void.
   */
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int index, seen = 0;
+	hash_node_t *temp;
 
 	printf("{");
 	index = 0;
@@ -19,6 +20,18 @@ void hash_table_print(const hash_table_t *ht)
 				printf(", ");
 			printf("'%s': '%s'", ht->array[index]->key, ht->array[index]->value);
 			seen++;
+			if (ht->array[index]->next != NULL)
+			{
+				temp = ht->array[index]->next;
+				while (temp)
+				{
+					if (seen >= 1)
+						printf(", ");
+					printf("'%s': '%s'", temp->key, temp->value);
+					seen++;
+					temp = temp->next;
+				}
+			}
 		}
 		index++;
 	}
