146e7bf5cccdc1f887a437f6ce2a55a7dade1141
ZeusGolohor <golohorzeusufuoma@gmail.com>
2023-03-16 16:38:34 -0700

    Write a function that converts a binary number to an unsigned int.
    Prototype: unsigned int binary_to_uint(const char *b);
    where b is pointing to a string of 0 and 1 chars
    Return: the converted number, or 0 if
    there is one or more chars in the string b that is not 0 or 1
    b is NULL

--- 0x14-bit_manipulation/0-main.c
+++ 0x14-bit_manipulation/0-main.c
@@ -10,9 +10,9 @@ int main(void)
 {
     unsigned int n;
 
-    n = binary_to_uint("1");
-    printf("%u\n", n);
-    n = binary_to_uint("101");
+   	n = binary_to_uint("1");
+   	printf("%u\n", n);
+   	n = binary_to_uint("101");
     printf("%u\n", n);
     n = binary_to_uint("1e01");
     printf("%u\n", n);
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -11,7 +11,9 @@ unsigned long int _pow_ulong(unsigned long int, unsigned long int);
 unsigned long int _pow_ulong_uint(unsigned long int, unsigned int);
 void print_binary(unsigned long int n);
 void get_highest_index(unsigned long int, unsigned long int *);
-int _putchar(char c);
+/** int _putchar(char c);*/
+void _putchar(char);
 int get_bit(unsigned long int n, unsigned int index);
 void get_highest_index_ulong_uint(unsigned long int, unsigned int *);
+unsigned int _pow(unsigned int, unsigned int);
 #endif
