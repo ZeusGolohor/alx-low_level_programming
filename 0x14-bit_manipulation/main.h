6dd2613f914962ac95b2594b1c48f85b95246407
ZeusGolohor <golohorzeusufuoma@gmail.com>
2023-03-23 15:34:27 -0700

    Write a function that sets the value of a bit to 0 at a given index.
    Prototype: int clear_bit(unsigned long int *n, unsigned int index);
    where index is the index, starting from 0 of the bit you want to set
    Returns: 1 if it worked, or -1 if an error occurred

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -15,4 +15,5 @@ int get_bit(unsigned long int , unsigned int);
 void get_highest_index_ulong_uint(unsigned long int, unsigned int *);
 unsigned int _pow(unsigned int, unsigned int);
 int set_bit(unsigned long int *, unsigned int);
+int clear_bit(unsigned long int *, unsigned int);
 #endif
