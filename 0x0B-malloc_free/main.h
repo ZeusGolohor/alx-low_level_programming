90137ffe4a7cfb2aaf4aed42e1a7d1e72d07a1f6
Golohor Ufuoma (Zeus) <golohorzeusufuoma@gmail.com>
2023-07-03 18:18:51 -0700

    Delete 0x0B-malloc_free directory

--- 0x0B-malloc_free/main.h
+++
@@ -1,7 +0,0 @@
-int _putchar(char c);
-char *create_array(unsigned int size, char c);
-char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
-void strasn(char *s1, char *s2, char *str, unsigned int len_s1, unsigned int len_s2, unsigned int len_total, unsigned int i, unsigned int x);
-int **alloc_grid(int width, int height);
-
