0f61b5eedefe941cf07346d7f40e594e376778f4
Zeus Golohor <golohorzeusufuoma@gmail.com>
2023-07-13 15:43:32 -0700

    Write a function that allocates memory using malloc.
    Prototype: void *malloc_checked(unsigned int b);
    Returns a pointer to the allocated memory
    if malloc fails, the malloc_checked function should cause normal process termination with a status value of 98

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * malloc_checked - A function that allocates memory using malloc.
+ * @b: size of the memory to allocate.
+ *
+ * Return: void*.
+ */
 void *malloc_checked(unsigned int b)
 {
 	void *temp;
