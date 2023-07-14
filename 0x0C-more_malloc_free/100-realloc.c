f1af52e82b1946b8951171ae0cdbb5f1999e7caa
Zeus Golohor <golohorzeusufuoma@gmail.com>
2023-07-14 16:37:00 -0700

    Write a function that reallocates a memory block using malloc and free
    Prototype: void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
    where ptr is a pointer to the memory previously allocated with a call to malloc: malloc(old_size)
    old_size is the size, in bytes, of the allocated space for ptr
    and new_size is the new size, in bytes of the new memory block
    The contents will be copied to the newly allocated space, in the range from the start of ptr up to the minimum of the old and new sizes
    If new_size > old_size, the “added” memory should not be initialized
    If new_size == old_size do not do anything and return ptr
    If ptr is NULL, then the call is equivalent to malloc(new_size), for all values of old_size and new_size
    If new_size is equal to zero, and ptr is not NULL, then the call is equivalent to free(ptr). Return NULL
    Don’t forget to free ptr when it makes sense
    FYI: The standard library provides a different function: realloc. Run man realloc to learn more.

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,6 +1,43 @@
 #include "main.h"
 
+/**
+ * _realloc - A function that reallocates a memory block using malloc and
+ * free.
+ * @ptr: pointer to the memory previously allocated with a call to
+ * malloc: malloc(old_size).
+ * @old_size: size of @ptr.
+ * @new_size: new size of @ptr.
+ *
+ * Return: void *.
+ */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	
+	char *temp, *_ptr = ptr;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+	{
+		temp = malloc(new_size);
+		if (temp == NULL)
+			return (NULL);
+	}
+	else
+	{
+		temp = malloc(old_size + new_size);
+		if (temp == NULL)
+			return (NULL);
+	}
+	for (i = 0; i < old_size; i++)
+		temp[i] = _ptr[i];
+	if (new_size < old_size)
+	{
+		for (i = old_size; i < new_size; i++)
+		{
+			temp[i] = 0;
+		}
+	}
+	free(ptr);
+	return (temp);
 }
