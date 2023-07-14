fb196c60c9fb73e3bcfedf1404a01b5e3c93036e
Zeus Golohor <golohorzeusufuoma@gmail.com>
2023-07-14 16:07:15 -0700

    Write a program that multiplies two positive numbers.
    Usage: mul num1 num2
    num1 and num2 will be passed in base 10
    Print the result, followed by a new line
    If the number of arguments is incorrect, print Error, followed by a new line, and exit with a status of 98
    num1 and num2 should only be composed of digits. If not, print Error, followed by a new line, and exit with a status of 98
    You are allowed to use more than 5 functions in your file
    You can use bc (man bc) to check your results.

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(int ac, char *av[])
+{
+	int i;
+	size_t total = 0;
+	unsigned int temp_size = 0, x;
+
+	if (ac != 3)
+	{
+		_print("Error\n");
+		exit(98);
+	}
+	for (i = 1; i < ac; i++)
+	{
+		temp_size = get_size(av[i]);
+		for (x = 0; x < temp_size; x++)
+		{
+			if ((av[i][x] >= '0') && (av[i][x] <= '9'))
+			{}
+			else
+			{
+				printf("Error: %c\n", av[i][x]);
+				exit(98);
+			}
+		}
+	}
+	total = atoi(av[1]) * atoi(av[2]);
+	printf("%ld\n", total);
+	return (0);
+}
+
+void _print(char *str)
+{
+	unsigned int len, i;
+
+	len = get_size(str);
+	for (i = 0; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,4 +6,7 @@ char *string_nconcat(char *, char *, unsigned int);
 unsigned int get_size(char *);
 void *_calloc(unsigned int, unsigned int);
 int *array_range(int, int);
+void *_realloc(void *, unsigned int, unsigned int);
+void _print(char *);
+int _putchar(char);
 #endif
