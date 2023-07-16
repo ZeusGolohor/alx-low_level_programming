8a573db3d17057695d42ff4ee52ffd9f457106f8
Zeus Golohor <golohorzeusufuoma@gmail.com>
2023-07-15 17:54:24 -0700

    Write a program that multiplies two positive numbers.
    Usage: mul num1 num2
    num1 and num2 will be passed in base 10
    Print the result, followed by a new line
    If the number of arguments is incorrect, print Error, followed by a new line, and exit with a status of 98
    num1 and num2 should only be composed of digits. If not, print Error, followed by a new line, and exit with a status of 98
    You are allowed to use more than 5 functions in your file
    You can use bc (man bc) to check your results."
    "

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -46,3 +46,20 @@ void _print(char *str)
 	}
 }
 
+/**
+ * get_size - Used to get the size of a string.
+ * @str: the string size to be gotten.
+ *
+ * Return: unsigned int.
+ */
+unsigned int get_size(char *str)
+{
+	unsigned int i, count = 0;
+
+	if (str == NULL)
+		return (0);
+	for (i = 0; str[i] != '\0'; i++)
+		count = count + 1;
+	return (count);
+}
+
