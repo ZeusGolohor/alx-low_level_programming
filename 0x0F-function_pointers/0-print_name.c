3a231d5cb7b5f15f246283eb6fb0acfc9c9dbace
Zeus Golohor <golohorzeusufuoma@gmail.com>
2023-07-19 20:42:34 -0700

    Write a function that prints a name.
    Prototype: void print_name(char *name, void (*f)(char *));

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - A function that prints a name.
+ * @name: string to be printed by a function pointer.
+ * @f: a function pointer.
+ *
+ * Return: void.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+ALX SE - 0x0F-function_pointers
