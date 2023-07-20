024faca6d6b1584473cf165d7b0b0781abd2d270
Zeus Golohor <golohorzeusufuoma@gmail.com>
2023-07-19 20:51:36 -0700

    Prototype: void array_iterator(int *array, size_t size, void (*action)(int)); where size is the size of the array and action is a pointer to the function you need to use

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - A function that executes a function given as a
+ * parameter on each element of an array.
+ * @array: array of integers to be passed to the pointer to a function.
+ * @size: size of @array.
+ * @action: a pointer to a function.
+ *
+ * Return: void.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		(*action)(array[i]);
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stddef.h>
 void print_name(char *, void (*f)(char *));
+void array_iterator(int *, size_t, void (*)(int));
 #endif
