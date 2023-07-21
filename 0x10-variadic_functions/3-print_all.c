d8b10f58b6fbd76e3e53b20cb5e67fb72303bb90
Zeus Golohor <golohorzeusufuoma@gmail.com>
2023-07-20 19:06:48 -0700

    Write a function that prints anything.
    Prototype: void print_all(const char * const format, ...);
    where format is a list of types of arguments passed to the function
    c: char
    i: integer
    f: float
    s: char * (if the string is NULL, print (nil) instead
    any other char should be ignored
    see example
    You are not allowed to use for, goto, ternary operator, else, do ... while
    You can use a maximum of
    2 while loops
    2 if
    You can declare a maximum of 9 variables
    You are allowed to use printf
    Print a new line at the end of your function

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,62 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - A  function that prints anything.
+ * @format: format used in printing the arguments passed to the function.
+ *
+ * Return: void.
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	va_list ap;
+	char *str;
+
+	va_start(ap, format);
+	while (format[i] != '\0')
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(ap, int));
+				check_separator(format, i);
+				break;
+			case 'i':
+				printf("%d", va_arg(ap, int));
+				check_separator(format, i);
+				break;
+			case 'f':
+				printf("%f", va_arg(ap, double));
+				check_separator(format, i);
+				break;
+			case 's':
+				str = va_arg(ap, char *);
+				if (str != NULL)
+				{
+					printf("%s", str);
+					check_separator(format, i);
+				}
+				else
+				{
+					printf("(nil)");
+					check_separator(format, i);
+				}
+				break;
+		}
+		i++;
+	}
+	printf("\n");
+}
+
+/**
+ * check_separator - A function used to check if output should be separated.
+ * @format: format used in printing the arguments passed to the function.
+ * @index: index at @format to check.
+ *
+ * Return: void.
+ */
+void check_separator(const char *format, int index)
+{
+	if (format[(index + 1)] != '\0')
+		printf(", ");
+}
