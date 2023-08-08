1321ba555c34471c9ce290b9ba6c013f284d95d9
golohorzeus <golohorzeusufuoma@gmail.com>
2023-08-07 18:24:57 -0700

    Write a function that reads a text file and prints it to the POSIX standard output.
    Prototype: ssize_t read_textfile(const char *filename, size_t letters);
    where letters is the number of letters it should read and print
    returns the actual number of letters it could read and print
    if the file can not be opened or read, return 0
    if filename is NULL return 0
    if write fails or does not write the expected amount of bytes, return 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * read_textfile - A function that reads a text file and prints it to the
+ * POSIX standard output.
+ * @filename: the file to be read.
+ * @letters: numbers of characters to read from @filename.
+ *
+ * Return: ssize_t.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file;
+	ssize_t i = 0;
+	size_t x = 0;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0);
+	if (letters != 0)
+	{
+		buffer = malloc((sizeof(char) * letters) + 1);
+		if (buffer == NULL)
+			return (0);
+		read(file, buffer, letters);
+	}
+	while ((x < letters) && ((buffer[i] != '\0') || (buffer[i] != EOF)))
+	{
+		_putchar(buffer[i]);
+		++i;
+		++x;
+	}
+	i = strlen(buffer);
+	return (i);
+}
+
+/**
+  * _putchar - Used to write to the stdout.
+  * @c: char
+  * Return: int.
+  */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <sys/types.h>
+ #include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+ssize_t read_textfile(const char *, size_t);
+int _putchar(char);
+#endif
