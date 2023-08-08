4138c29bba8d84536dcd25163b8848fde7a78ee6
golohorzeus <golohorzeusufuoma@gmail.com>
2023-08-07 19:11:55 -0700

    Write a function that reads a text file and prints it to the POSIX standard output.
    Prototype: ssize_t read_textfile(const char *filename, size_t letters);
    where letters is the number of letters it should read and print
    returns the actual number of letters it could read and print
    if the file can not be opened or read, return 0
    if filename is NULL return 0
    if write fails or does not write the expected amount of bytes, return 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * read_textfile - A function that reads a text file and prints it to the
@@ -20,18 +21,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	file = open(filename, O_RDONLY);
 	if (file == -1)
 		return (0);
-/*	if (letters != 0)
-	{
-		buffer = malloc((sizeof(char) * letters) + 1);
-		if (buffer == NULL)
-			return (0);
-		r = read(file, buffer, letters);
-		if (r == -1)
-			return (0);
-		buffer[(letters + 1)] = '\0';
-		write(STDOUT_FILENO, buffer, letters);
-		free(buffer);
-	}*/
 	buffer = malloc((sizeof(char) * letters));
 	r = read(file, buffer, letters);
 	r = write(STDOUT_FILENO, buffer, r);
