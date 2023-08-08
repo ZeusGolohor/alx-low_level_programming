720b5192a9338ad30311240c02ebabfbcc77500a
golohorzeus <golohorzeusufuoma@gmail.com>
2023-08-07 18:51:37 -0700

    Write a function that reads a text file and prints it to the POSIX standard output.
    Prototype: ssize_t read_textfile(const char *filename, size_t letters);
    where letters is the number of letters it should read and print
    returns the actual number of letters it could read and print
    if the file can not be opened or read, return 0
    if filename is NULL return 0
    if write fails or does not write the expected amount of bytes, return 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,6 +8,7 @@
  *
  * Return: ssize_t.
  */
+/**
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file;
@@ -37,6 +38,41 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		free(buffer);
 	}
 	close(file);
+	return (r);
+}
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buf;
+	ssize_t r;
+	int i;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == (-1))
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters + 1));
+	if (buf == NULL)
+		return (0);
+
+	r = read(fd, buf, letters);
+	if (r == (-1))
+		return (0);
+
+	*(buf + (letters + 1)) = '\0';
+	i = 0;
+	while (*(buf + i) != '\0')
+	{
+		_putchar(*(buf + i));
+		i++;
+	}
+	free(buf);
+	close(fd);
 	return (r);
 }
 
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <sys/types.h>
- #include <sys/stat.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
