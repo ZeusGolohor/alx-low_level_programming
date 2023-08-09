a5cbb68d8b7e8398ed9efb52ebb282bd630b05d8
golohorzeus <golohorzeusufuoma@gmail.com>
2023-08-08 20:10:13 -0700

    Write a function that appends text at the end of a file.
    Prototype: int append_text_to_file(const char *filename, char *text_content);
    where filename is the name of the file and text_content is the NULL terminated string to add at the end of the file
    Return: 1 on success and -1 on failure
    Do not create the file if it does not exist
    If filename is NULL return -1
    If text_content is NULL, do not add anything to the file. Return 1 if the file exists and -1 if the file does not exist or if you do not have the required permissions to write the file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,37 +1,27 @@
 #include "main.h"
 
 /**
-  * append_text_to_file - Appends text to the end of a file.
-  * @filename: const char *
-  * @text_content: char
-  * Return: int
-  */
-
+ * append_text_to_file - A function that appends text at the end of a file.
+ * @filename: the to be modified.
+ * @text_content: text to be added to @filename.
+ *
+ * Return: int.
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, len;
+	int fd;
 
 	if (filename == NULL)
 		return (-1);
-
-	if (access(filename, F_OK | W_OK) == 0)
-	{
-		fd = open(filename, O_WRONLY | O_APPEND);
-	}
-	else
-		return (-1);
-
-	if (fd == (-1))
-		return (-1);
-
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len] != '\0';)
-			len++;
-
-		write(fd, text_content, len);
+		if (access(filename, F_OK | W_OK) == -1)
+			return (-1);
+		fd = open(filename, O_WRONLY);
+		if (fd != -1)
+			write(fd, text_content, strlen(text_content));
+		else
+			return (-1);
 	}
-	close(fd);
 	return (1);
 }
-
