9a7d35cb33f4f5d3e3c19235cfd76fdc5b955946
golohorzeus <golohorzeusufuoma@gmail.com>
2023-08-08 19:34:59 -0700

    Create a function that creates a file.
    Prototype: int create_file(const char *filename, char *text_content);
    where filename is the name of the file to create and text_content is a NULL terminated string to write to the file
    Returns: 1 on success, -1 on failure (file can not be created, file can not be written, write “fails”, etc…)
    The created file must have those permissions: rw-------. If the file already exists, do not change the permissions.
    if the file already exists, truncate it
    if filename is NULL return -1
    if text_content is NULL create an empty file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,17 +1,26 @@
 #include "main.h"
 
+/**
+ * create_file - A  function that creates a file.
+ * @filename: name of the file to be created or accessed.
+ * @text_content: content to be added to @filename.
+ *
+ * Return: int.
+ */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, len = 0;
 
-	if ((filename == NULL) && (text_content == NULL))
+	if (filename == NULL)
 		return (-1);
 	if (access(filename, F_OK) == 0)
-		fd = open(filename, O_WRONLY);
+		fd = open(filename, O_WRONLY | O_TRUNC);
 	else
-		fd = open(filename, O_CREAT, O_WRONLY, 0600);
-
+		fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	if (text_content != NULL)
+		len = write(fd, text_content, strlen(text_content));
+	else
+		len = write(fd, "", 0);
 	close(fd);
-
-	return (0);	
+	return (len);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 ssize_t read_textfile(const char *, size_t);
 int create_file(const char *, char *);
 int append_text_to_file(const char *, char *);
