8c06c2d5a0570f2ce95c3901214f7183324ab5bc
golohorzeus <golohorzeusufuoma@gmail.com>
2023-08-31 12:41:52 -0700

    Write a function that sets the value of a bit to 0 at a given index.
    Prototype: int clear_bit(unsigned long int *n, unsigned int index);
    where index is the index, starting from 0 of the bit you want to set
    Returns: 1 if it worked, or -1 if an error occurred

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+/**
+ * clear_bit - A function that sets the value of a bit to 0 at a given index.
+ * @n: the number which bit is to be set.
+ * @index: index of the bit to set.
+ *
+ * Return: int.
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int _n = *n, total = 0;
+	long int i = 0, seen = 0;
+
+	while (total < _n)
+	{
+		total = total + _pow_lint(2, i);
+		++i;
+	}
+	--i;
+	total = 0;
+	while (i >= 0)
+	{
+		if ((total + _pow_lint(2, i)) <= _n)
+		{
+			if (i == index)
+				seen = 1;
+			total = total + _pow_lint(2, i);
+		}
+		--i;
+	}
+	if (seen == 1)
+		total = total - _pow_lint(2, index);
+	*n = total;
+	if (_pow_lint(2, index) == 0)
+		return (-1);
+	return (1);
+}
+
+/**
+ * _pow_lint - A function to get the result of a number raised to the power of
+ * another number.
+ * @number: the number to be multiplied by @power times.
+ * @power: the number of times to multiply a number by itself.
+ *
+ * Return: unsigned long int.
+ */
+unsigned long int _pow_lint(unsigned long int number, long int power)
+{
+	unsigned long int pow = 1;
+
+	while (power > 0)
+	{
+		pow = number * pow;
+		--power;
+	}
+	return (pow);
+}
+
