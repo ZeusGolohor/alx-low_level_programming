1ccbfa73259475aa65fed6cdd8219d4d5aa22560
root <zeusgolohor@gmail.com>
2024-02-06 18:45:33 -0800

    Write a function that searches for a value in a sorted array of integers using the Binary search algorithm
    Prototype : int binary_search(int *array, size_t size, int value);
    Where array is a pointer to the first element of the array to search in
    size is the number of elements in array
    And value is the value to search for
    Your function must return the index where value is located
    You can assume that array will be sorted in ascending order
    You can assume that value won’t appear more than once in array
    If value is not present in array or if array is NULL, your function must return -1
    You must print the array being searched every time it changes. (e.g. at the beginning and when you search a subarray) (See example)

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,19 +11,59 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-	int m = 0;
-	size_t l = 0, r = (size - 1);
+	size_t m = 0, i = 0, p = 0;
+	size_t l = 0, r = 0;
 
-	while (l > r)
+	if ((size == l) || (array == NULL))
+		return (-1);
+	r = (size - 1);
+	printf("Searching in array: ");
+	i = 0;
+	while (i <= r)
+	{
+		printf("%d", array[i]);
+		if ((i + 1) <= r)
+			printf(", ");
+		else
+			printf("\n");
+		i = i + 1;
+	}
+	while (l <= r)
 	{
 		m = l + (r - l)/2;
-
 		if (array[m] == value)
 			return (m);
-		if (array[m] < value)
+		if (array[m] <= value)
+		{
 			l = m + 1;
-		if (array[m] > value)
+			i = l;
+			if (i <= r)
+                        	printf("Searching in array: ");
+			while (i <= r)
+			{
+				printf("%d", array[i]);
+				if ((i + 1) <= r)
+					printf(", ");
+				else
+					printf("\n");
+				i = i + 1;
+			}
+		}
+		else
+		{
 			r = m - 1;
+			printf("Searching in array: ");
+			i = l;
+			while (i <= r)
+			{
+				printf("%d", array[i]);
+				if ((i + 1) <= r)
+					printf(", ");
+				else
+					printf("\n");
+				i = i + 1;
+			}
+		}
 	}
 	return (-1);
 }
